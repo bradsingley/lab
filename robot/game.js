4c00396494a19a0761c905dd740a704c8df95fcb
Brad Singley <bradsingley@gmail.com>
2025-11-12 23:41:27 -0800

    Add video o to available videos list

--- robot/game.js
+++ robot/game.js
@@ -3,7 +3,7 @@ import { captions } from './captions.js';
 // Configuration
 const VIDEO_PATH = 'videos/';
 // Only include letters that have video files
-const availableVideos = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'n', 'p', 'q', 'r', 't', 'v', 'w', 'x', 'y', 'z', 'space'];
+const availableVideos = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'n', 'o', 'p', 'q', 'r', 't', 'v', 'w', 'x', 'y', 'z', 'space'];
 const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 // DOM elements - will be set after DOM loads
