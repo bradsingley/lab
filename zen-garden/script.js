ca69752fea985ca7e5e62e1bce18ca3b1f5f97ee
Brad Singley <bradsingley@gmail.com>
2026-01-18 01:02:27 -0800

    Update script.js

--- zen-garden/script.js
+++ zen-garden/script.js
@@ -389,11 +389,11 @@ class ZenGardenApp {
     const sun = new THREE.DirectionalLight(0xffddaa, 1.4);
     sun.position.set(20, 12, -8);
     sun.castShadow = true;
-    sun.shadow.mapSize.set(2048, 2048);
+    sun.shadow.mapSize.set(4096, 4096);
     sun.shadow.camera.left = sun.shadow.camera.bottom = -25;
     sun.shadow.camera.right = sun.shadow.camera.top = 25;
-    sun.shadow.bias = -0.0001;
-    sun.shadow.normalBias = 0.02;
+    sun.shadow.bias = -0.001;
+    sun.shadow.normalBias = 0.01;
     this.scene.add(sun);
     this.scene.add(new THREE.DirectionalLight(0x6688aa, 0.25).translateX(-5).translateY(4));
     
